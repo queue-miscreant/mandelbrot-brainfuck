30ef62329a37ab6061cb230cbf98a9fd22ad8fc9
Cube <cubebert549@gmail.com>
2017-08-24 22:41:48 -0500

    commiting latest changes to my (bad) interpreter; upped the number of iterations to 9

--- bfint.py
+++ bfint.py
@@ -3,20 +3,34 @@ import sys
 import os
 
 class interpreter:
-	strip = [0 for i in range(300)] #data stored here
-	pointer = 0 #where in the data
-
-	program = ""	#program stored here
-	instruction = 0 #where in the program
-
-	bracketPositions = []
-	breakpoint = False
-	breakpoint2 = False
-
 	def __init__(self,program,debug,always):
+		self.debug = debug		#debugger active
+		self.always = always	#always show the strips
+
+		self.strip = [0 for i in range(300)] #data stored here
+		self.pointer = 0 #where in the data
 		self.program = program
-		self.debug = debug
-		self.always = always
+		self.instruction = 0 #where in the program
+
+		self.breakpoint = False		#breakpoints. ! sets breakpoints and turns on breakpointing for ?
+		self.breakpoint2 = False
+
+		self.lastCommand = "continue"
+		#line buffered, not system default
+		self.outfile = os.fdopen(sys.stdout.fileno(),'w',1)
+
+		self.bracketPositions = []
+
+	def computeJumps(self):
+		self.jumps = {}
+		jumpstack = []
+		for i,j in enumerate(self.program):
+			if j == '[':
+				jumpstack.append(i)
+			elif j == ']':
+				jump = jumpstack.pop()
+				self.jumps[i] = jump
+				self.jumps[jump] = i
 
 	#if ouput is going to be 8 cells in either direction = (8*2)+1 numbers * 3 spaces = 51 columns
 	#the cell with focus needs to be centered
@@ -24,92 +38,120 @@ class interpreter:
 	#do that, so we don't pad spaces, we just need to check for negatives
 	def showStrip(self):
 		if self.always or self.breakpoint:
-			output = []
-			celnos = []
-			ran = 16
-			if self.pointer < 7:
-				ran -= 8-self.pointer
-				output = self.strip[self.pointer-7:]
-	#			celnos = [len(self.strip)-i for i in reversed(range(7-self.pointer))]
-	#			celnos.extend(range(0,self.pointer))
-			print(('   '*9) + '  v')
+			print(end="\x1b[3A")
 			start = max(self.pointer-7,0)
-			output.extend(self.strip[start:start+ran])
-	#		celnos.extend([i for i in range(self.pointer+1,self.pointer+8)])
-	#		print('│'.join(['%3.f' % i for i in celnos]))
-			print('│'.join(['%3.f' % i for i in output]))
-			output = ''
-			print('    '*(max(0,7-self.instruction)) + '│'.join([' %s ' % self.program[i]
-				for i in range(max(0,self.instruction-8),min(len(self.program),self.instruction+8))]),end="\x1b[K\n")
+			ran = max(7-self.pointer,0)
+			print(('   '*9) + '  v')
+			print('    '*(ran) + '│'.join(['%3.f' % i for i in self.strip[start:start+16-ran]]),end="\x1b[K\n")
+			start = max(self.instruction-7,0)
+			ran = max(7-self.instruction,0)
+			print('    '*(ran) + '│'.join([' %s ' % i for i in self.program[start:start+16-ran]]),end="\x1b[K\n")
 		if self.breakpoint:
 			print(self.instruction,self.pointer)
 			a = input()
-			if a:
-				if 'step'.find(a) == 0:
-					pass
-				elif 'next'.find(a) == 0:
-					self.pointer += 1
-					self.showStrip()
-				elif 'previous'.find(a) == 0:
-					self.pointer -= 1
-					self.showStrip()
-				else:
-					self.breakpoint = False
-			else:
-				self.breakpoint = False
-					
 			print(end="\x1b[2F")
-				
-		print(end="\x1b[3F")
+			if not a: a = self.lastCommand
+			if 'step'.find(a) == 0:
+				self.lastCommand = "step"
+				pass
+			elif 'next'.find(a) == 0:
+				self.lastCommand = "next"
+				self.pointer += 1
+				self.showStrip()
+			elif 'previous'.find(a) == 0:
+				self.lastCommand = "previous"
+				self.pointer -= 1
+				self.showStrip()
+			elif 'continue'.find(a) == 0:
+				self.lastCommand = "continue"
+				self.breakpoint = False
+			else:
+				self.lastCommand = "continue"
 
-	def interpret(self,ins):
+	def interpret(self):
+		ins = self.program[self.instruction]
 		if ins == '+':
 			self.strip[self.pointer] = (self.strip[self.pointer] + 1) % 256
-		if ins == '-':
+		elif ins == '-':
 			self.strip[self.pointer] = (self.strip[self.pointer] - 1) % 256
-		if ins == '>':
+		elif ins == '>':
 			self.pointer = (self.pointer + 1) % len(self.strip)
-		if ins == '<':
+		elif ins == '<':
 			self.pointer = (self.pointer - 1) % len(self.strip)
-		if ins == '[':
+		elif ins == '[':
 			#if i cared, i'd compute matching brackets
 			if not self.strip[self.pointer]:
-				counter = 1
+				counter = 0
 				for i in range(self.instruction,len(self.program)):
 					if self.program[i] == '[':
 						counter += 1
 					elif self.program[i] == ']':
 						counter -= 1
 						if not counter:
-							self.instruction = i+1
+							self.instruction = i
 							break
 			else:
 				self.bracketPositions.append(self.instruction-1)
-		if ins == ']':
+		elif ins == ']':
 			self.instruction = self.bracketPositions.pop()
-		if ins == '!': #BREAK
+		elif ins == '!': #BREAK
 			self.breakpoint = True
 			self.breakpoint2 = True
-		if ins == '?': #BREAK
+		elif ins == '?': #BREAK
 			self.breakpoint = self.breakpoint2
-		if ins == '.':
-			if self.debug:
-				with open("debug",'a') as a:
-					a.write(chr(self.strip[self.pointer]))
-				return
-			print(end=chr(self.strip[self.pointer]))
+		elif ins == '.':
+			print(end=chr(self.strip[self.pointer]),file=self.outfile)
 
-	def start(self):
+	def opt_interpret(self):
+		ins = self.program[self.instruction]
+		if ins == '+':
+			self.strip[self.pointer] += 1
+		elif ins == '-':
+			self.strip[self.pointer] -= 1
+		elif ins == '>':
+			self.pointer = (self.pointer + 1) % len(self.strip)
+		elif ins == '<':
+			self.pointer = (self.pointer - 1) % len(self.strip)
+		elif ins == '[':
+			if not self.strip[self.pointer]:
+				self.instruction = self.jumps[self.instruction]
+		elif ins == ']':
+			if self.strip[self.pointer]:
+				self.instruction = self.jumps[self.instruction]-1
+			#skip consecutive ]s
+			else:
+				for i in range(self.instruction+1,len(self.program)):
+					if self.program[i] != ']':
+						self.instruction = i-1
+						break
+		elif ins == '.':
+			print(end=chr(self.strip[self.pointer]),file=self.outfile,flush=True)
+
+	def start(self,optimize):
+		if self.debug:
+			print("\n\n")
+			self.outfile = open("debug",'a')
 		try :
-			while self.instruction < len(self.program):
+			if optimize:
+				self.computeJumps()
+				while self.instruction < len(self.program):
+					if self.debug:
+						self.showStrip()
+					self.opt_interpret()
+					self.instruction += 1
 				if self.debug:
 					self.showStrip()
-				self.instruction += 1
-				self.interpret(self.program[self.instruction-1])
-			if isdebug:
-				self.showStrip()
-				print("\n\n")
+			else:
+				while self.instruction < len(self.program):
+					if self.debug:
+						self.showStrip()
+					self.interpret()
+					self.instruction += 1
+				if self.debug:
+					self.showStrip()
 		except KeyboardInterrupt: pass
+		if self.debug:
+			self.outfile.close()
 
 if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
 	program = ""
@@ -117,12 +159,10 @@ if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
 		program = a.read()
 	isdebug = "-d" in sys.argv
 	always = "-s" in sys.argv
+	optimize = "-o" in sys.argv
 	progmin = [i for i in program if i in '+-<>[]!?.']
 	if "-min" in sys.argv:
 		with open(sys.argv[1]+"min",'w') as a:
 			a.write(''.join(progmin))
 	else:
-		interpreter(progmin,isdebug,always).start()
-				
-else:
-	exit()
+		interpreter(progmin,isdebug,always).start(optimize)
